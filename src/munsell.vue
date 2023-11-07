ac8e2b6744d1f757384d119cf1b11b63b3794645
Miquel-BR <miquel.domingo@bcnregional.com>
2023-11-07 14:52:07 +0100

    dibuix categories colors

--- src/munsell.vue
+++ src/munsell.vue
@@ -1,8 +1,11 @@
 <template>
     <div class="content is-clearfix">
         <div>Delta: {{delta.toFixed(2)}} Color: {{color.rgb}} Munsell: {{munsellColor}} || Hue: {{indX * 4.5}} Saturació: {{((indFulla * 0.05) + 1).toFixed(2)}} Luminositi: {{(indY * 0.05) + 1}}  Fulla: {{indFulla + 1}}/18 X:{{indX + 1}} Y:{{indY + 1}}</div>
-        <div class="content is-clearfix" v-for="(va, idx) in valors[this.indFulla]" style="margin-bottom: 0;">
+        <div class="content is-clearfix" v-for="(va, idx) in valors[this.indFulla]" style="margin-bottom: 0;position:relative;">
             <div v-for="(v, iddx) in va" style="width: 15px;height: 15px;" class="is-pulled-left" :style="estil(v, idx, iddx)"></div>
+            <div style="position: absolute; left: 0;top:0">
+                <div v-for="(vv, idddx) in categoriesArray" style="width: 15px;height: 15px;background-color: transparent;" :style="catEstil(vv)" class="is-pulled-left"></div>
+            </div>
         </div>
     </div>
 </template>
@@ -34,6 +37,14 @@ export default {
             indX: 0,
             delta: 100,
             munsellColor: '',
+            categoriesArray: [13,5,5,5,5,5,5,7,9,1,1,1,5,7,15,11,13,5,7,9,1,7,11,15,11,13,5,5,5,5,5,5,5,5,5,5,5,5,5,7,
+                9,1,1,7,9,1,3,13,0,0,0,6,9,1,1,0,1,1,3,12,6,13,4,5,4,5,5,5,5,7,13,1,1,1,3,9,1,1,1,3,
+                8,0,6,9,0,0,0,3,8,4,6,9,0,0,0,0,0,0,0,1,1,7,9,1,1,1,1,1,1,1,3,8,0,0,2,8,0,0,0,2,
+                8,6,11,12,4,0,4,6,14,13,1,0,0,0,0,0,0,0,0,0,2,9,0,0,0,0,0,0,0,0,2,8,0,0,2,12,0,0,0,2,
+                10,9,0,1,1,14,9,1,1,3,8,0,0,0,0,0,0,0,0,0,2,8,0,0,0,0,0,0,0,0,2,8,0,0,0,3,12,0,0,2,
+                10,9,0,0,6,9,0,0,4,6,12,4,4,4,0,0,0,0,0,0,2,12,4,4,4,0,0,0,0,0,6,8,0,0,0,0,3,12,4,6,
+                13,4,4,6,13,4,4,6,9,1,1,1,1,3,12,4,4,4,4,4,4,7,9,3,15,12,4,4,4,6,13,4,4,4,4,4,4,5,5,7,
+                13,5,5,5,5,5,5,5,4,4,4,4,4,4,5,5,5,5,5,5,5,5,4,4,5,5,5,5,5,7,15,13,5,5,5,5,5,5,5,7]
         }
     },
     computed: {
@@ -48,6 +59,40 @@ export default {
         },
     },
     methods:{
+        catEstil(v){
+            let retorn = ''
+            let bin = this.dec2bin(v);
+            if(bin.length < 4){
+                if(bin.length == 3){
+                    bin = '0'+bin;
+                } else if(bin.length == 2){
+                    bin = '00'+bin;
+                } else {
+                    bin = '000'+bin;
+                }
+            }
+            let idx = 3;
+            for(let b of bin){
+                if(idx == 0 && b == '1') {
+                    retorn = retorn + "border-top:1px solid white;";
+                }
+                if(idx == 1 && b == '1') {
+                    retorn = retorn + "border-right:1px solid white;";
+                }
+                if(idx == 2 && b == '1') {
+                    retorn = retorn + "border-bottom:1px solid white;";
+                }
+                if(idx == 3 && b == '1') {
+                    retorn = retorn + "border-left:1px solid white;";
+                }
+                idx = idx - 1;
+            }
+            console.log(v, retorn);
+            return retorn;
+        },
+        dec2bin(dec) {
+            return (dec >>> 0).toString(2);
+        },
         estil(col, idx, iddx){
             let retorn = {'background-color': col};
             if(this.indX === iddx && this.indY === idx){
@@ -67,7 +112,6 @@ export default {
                     for(let [xx, x] of y.entries()){
                         let delta = Chroma.deltaE(this.colorProp, x);
                         if(delta < this.delta){
-                            console.log(delta, this.delta);
                             this.delta = delta;
                             this.indFulla = ffulla;
                             this.indY = yy;
@@ -103,9 +147,7 @@ export default {
             self.valors.push(temp2);
         }
         console.log(this.valors);
-
         this.find();
-
     }
 }
 </script>
